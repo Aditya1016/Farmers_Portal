b053b5a9ecd9a6ec281486d51d0987e22fffca1f
Aditya Sinha <iamaditya.sinha16@gmail.com>
2025-05-21 11:30:02 +0530

    Minor chnages for pushing to prod

--- Backend/app.js
+++ Backend/app.js
@@ -13,15 +13,28 @@ import cookieParser from 'cookie-parser'
 import arcjetMiddleware from './middlewares/arcjet.middleware.js'
 const app = express()
 
+import helmet from 'helmet';
+app.use(helmet());
+
 app.use(cors({
     origin: CORS_ORIGIN,
     credentials: true
 }))
 
+import morgan from 'morgan';
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+} else {
+  app.use(morgan('combined')); // better for production
+}
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 
+app.set('trust proxy', 1);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
